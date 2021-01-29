65c716d59065ae01f9cdc82fe331629cee5b2416
nmparfait <mbamu.developer@gmail.com>
2021-01-29 20:35:05 +0000

    remove 11 and add 1

--- main.py
+++ main.py
@@ -15,4 +15,20 @@ computer_cards = []
 for _ in range(2):
   user_cards.append(deal_card())
   computer_cards.append(deal_card())
+
+#Create a function called calculate_score() that takes a List of cards as input 
+#and returns the score. 
+#Look up the sum() function to help you do this.
+
+def calculate_score(cards):
+  # Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
   
+  # Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+  
+  if 11 in cards and sum(cards) > 21:
+    cards.remove(11)
+    cards.append(1)
+
+  return sum(cards)
