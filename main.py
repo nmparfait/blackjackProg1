0b7cfb4a5ee3e016e14406dfa56f79eef16e7c01
nmparfait <mbamu.developer@gmail.com>
2021-01-29 21:18:12 +0000

    can print the user and computer score

--- main.py
+++ main.py
@@ -8,19 +8,8 @@ def deal_card():
   card = random.choice(cards)
   return card
 
-#Deal the user and computer 2 cards using deal_card
-user_cards = []
-computer_cards = []
-
-for _ in range(2):
-  user_cards.append(deal_card())
-  computer_cards.append(deal_card())
-
-#Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
-
 def calculate_score(cards):
+  """Take a list of cards and return the score calculated from the cards"""
   # Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
   if sum(cards) == 21 and len(cards) == 2:
     return 0
@@ -32,3 +21,26 @@ def calculate_score(cards):
     cards.append(1)
 
   return sum(cards)
+
+#Deal the user and computer 2 cards using deal_card
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+  user_cards.append(deal_card())
+  computer_cards.append(deal_card())
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+
+print(f"   Your cards: {user_cards}, current score: {user_score}")
+print(f"   Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21: 
+  is_game_over = True
+
+#Create a function called calculate_score() that takes a List of cards as input 
+#and returns the score. 
+#Look up the sum() function to help you do this.
+
